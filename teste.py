890796f37b0a71061b3cb8901da116682eb50abc
Murilo <murilosilva.felipe@gmail.com>
2018-06-04 18:53:15 -0400

    iniciando projeto

---
+++ teste.py
@@ -0,0 +1,9 @@
+import pytest
+from principal import multiplicar
+
+
+def teste_multiplicar():
+    assert multiplicar(2,3)== 6
+    assert multiplicar(4, 3) == 12
+    assert multiplicar(4, 4) == 16
+    assert multiplicar(5, 7) == 35
