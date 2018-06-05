cdd20ea2c4ba36707c3d5c645f067aa9454a5eb4
Murilo <murilosilva.felipe@gmail.com>
2018-06-04 20:28:45 -0400

    ok

--- teste.py
+++ teste.py
@@ -6,4 +6,4 @@ def teste_multiplicar():
     assert multiplicar(2,3)== 6
     assert multiplicar(4, 3) == 12
     assert multiplicar(4, 4) == 16
-    assert multiplicar(5, 7) == 37
+    assert multiplicar(5, 7) == 35
